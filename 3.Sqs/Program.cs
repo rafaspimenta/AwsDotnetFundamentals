6f67b94dc99c1ff9a9e059a325b15ef3fc5af4f8
Rafael Pimenta <rafa.spimenta@gmail.com>
2024-01-15 20:13:24 -0300

    Creating a message publisher to AWS SQS

--- 3.Sqs/Program.cs
+++ 3.Sqs/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace _3.Sqs
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Text.Json;
+
+namespace _3.Sqs;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        var awsAccessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY", EnvironmentVariableTarget.Machine);
+        var awsSecretKey = Environment.GetEnvironmentVariable("AWS_SECRET_KEY", EnvironmentVariableTarget.Machine);
+
+        var credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+
+        var sqsClient = new AmazonSQSClient(credentials, RegionEndpoint.USEast2);
+
+        var customer = new CustomerCreated()
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Id = Guid.NewGuid(),
+            Email = "rafa.spimenta@gmail.com",
+            FullName = "Rafael Pimenta",
+            DateOfBirth = new DateTime(1986, 6, 12),
+            GitHubUsername = "rafaspimenta"
+        };
+
+        var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+        var sendMessageRequest = new SendMessageRequest()
+        {
+            QueueUrl = queueUrlResponse.QueueUrl,
+            MessageBody = JsonSerializer.Serialize(customer),
+            MessageAttributes = new Dictionary<string, MessageAttributeValue>
+            {
+                {
+                    "MessageType", new MessageAttributeValue()
+                    {
+                        DataType = "String",
+                        StringValue = nameof(CustomerCreated)
+                    }
+
+                }
+            }
+        };
+
+        var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+        Console.WriteLine(response.ToString());
+
     }
 }
